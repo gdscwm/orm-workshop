447426d07a6174cd4f8042d4e9ea9f449b2c3cef
cropthecoder <myracropper32@gmail.com>
2024-09-05 19:03:14 -0400

    workshop

---
+++ workshop1/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+
+
+engine = create_engine('sqlite:///example.db')
+Base.metadata.create_all(engine)
